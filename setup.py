d3d5e8de13ea33225ed154a185cf08273dbd1f43
rushhan <63231544+rushhan@users.noreply.github.com>
2021-01-11 01:51:03 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='sqrtlasso',
+      version='0.1',
+      description='Python package for sqrt lasso',
+      url='https://github.com/rushhan/sqrtlasso',
+      author='rushhan',
+      author_email='---',
+      license='MIT',
+      packages=['sqrtlasso'],
+      zip_safe=False)
